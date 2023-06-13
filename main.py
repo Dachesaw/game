eb32769b434600355198a41cf13b9164409e9907
Kaoru <topgearchesak@gmail.com>
2023-06-13 20:31:29 +0300

    Implemented control on the arrows

--- main.py
+++ main.py
@@ -35,9 +35,24 @@ class AliensInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                elif event.key == pygame.K_UP:
+                    self.ship.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = True
+                
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                elif event.key == pygame.K_UP:
+                    self.ship.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = False
+                
+            
 
     def _update_screen(self):
         """Обновляет изображение на экране и отображает новый экран"""
