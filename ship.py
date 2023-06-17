59066e04d532326694043df341cf9dba3b27abb3
Kaoru <topgearchesak@gmail.com>
2023-06-17 23:11:15 +0300

    ограничение движения корабля по разрешению экрана

--- ship.py
+++ ship.py
@@ -25,9 +25,9 @@ class Ship():
     def update(self):
         """обновляет позицию корабля с учетом флага"""
         
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         if self.moving_up:
             self.y -= self.settings.ship_speed
