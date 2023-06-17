c1ce86b6ade4a06d7c79e45a79999e9176c7d834
Kaoru <topgearchesak@gmail.com>
2023-06-17 23:22:24 +0300

    adding fullscreen

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class AliensInvasion:
         """Инициализирует игру и создает игровые ресурсы"""
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_widht, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_widht = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         self.bg_color = (self.settings.bg_color)
