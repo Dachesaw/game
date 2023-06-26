d1aab7cb7eb0702a5b57d61fcd1f3bbe272720b4
Dachesaw <123497362+Dachesaw@users.noreply.github.com>
2023-06-27 01:30:37 +0300

    extend update, draw_bullet

--- bullet.py
+++ bullet.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pygame
 from pygame.sprite import _Group, Sprite
 
@@ -17,3 +18,16 @@ class Bullet(Sprite):
         
         # позиция пули в вещесвенной переменной
         self.y = float(self.rect.y)
+    
+    
+    def update(self):
+        """Перемещает снаряд по экрану"""
+        # Обновление позиции снаряда в вещественном формате
+        self.y -= self.settings.bullet_speed
+        # Обновление позиции прямоугольника
+        self.rect.y = self.y
+        
+    
+    def draw_bullet(self):
+        """Вывод снаряда на экран"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
